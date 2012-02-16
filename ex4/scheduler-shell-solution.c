bba025a579bccf8776081079bb88c3d88d6bf5a5
Dionysis Zindros <dionyziz@gmail.com>
2012-02-16 13:33:41 +0000

    fix tabs

--- ex4/scheduler-shell-solution.c
+++ ex4/scheduler-shell-solution.c
@@ -44,7 +44,7 @@ sched_print_tasks(void)
             printf("\n");
         }
     }
-	// assert(0 && "Please fill me!");
+    // assert(0 && "Please fill me!");
 }
 
 /* Send SIGKILL to a task determined by the value of its
@@ -57,9 +57,9 @@ sched_kill_task_by_id(int id)
     assert(alive[id]);
     fprintf(stderr, "Killing process %i with pid %i.\n", id, childid[id]);
     kill(childid[id], SIGKILL);
-	// assert(0 && "Please fill me!");
+    // assert(0 && "Please fill me!");
     return 0;
-	// return -ENOSYS;
+    // return -ENOSYS;
 }
 
 static void
@@ -102,24 +102,24 @@ sched_create_task(char *executable)
             return;
         }
     }
-	// assert(0 && "Please fill me!");
+    // assert(0 && "Please fill me!");
 } 
 
 /* Process requests by the shell.  */
 static int
 process_request(struct request_struct *rq)
 {
-	switch (rq->request_no) {
-		case REQ_PRINT_TASKS:
-			sched_print_tasks();
-			return 0;
+    switch (rq->request_no) {
+        case REQ_PRINT_TASKS:
+            sched_print_tasks();
+            return 0;
 
-		case REQ_KILL_TASK:
-			return sched_kill_task_by_id(rq->task_arg);
+        case REQ_KILL_TASK:
+            return sched_kill_task_by_id(rq->task_arg);
 
-		case REQ_EXEC_TASK:
-			sched_create_task(rq->exec_task_arg);
-			return 0;
+        case REQ_EXEC_TASK:
+            sched_create_task(rq->exec_task_arg);
+            return 0;
 
         case REQ_HIGH_TASK:
             sched_high_priority(rq->task_arg);
@@ -129,9 +129,9 @@ process_request(struct request_struct *rq)
             sched_low_priority(rq->task_arg);
             return 0;
 
-		default:
-			return -ENOSYS;
-	}
+        default:
+            return -ENOSYS;
+    }
 }
 
 /* SIGALRM handler: Gets called whenever an alarm goes off.
@@ -144,7 +144,7 @@ sigalrm_handler(int signum)
 {
     alarm(SCHED_TQ_SEC);
     kill(childid[current], SIGSTOP);
-	// assert(0 && "Please fill me!");
+    // assert(0 && "Please fill me!");
 }
 
 /* SIGCHLD handler: Gets called whenever a process is stopped,
@@ -157,8 +157,8 @@ sigalrm_handler(int signum)
 static void
 sigchld_handler(int signum)
 {
-	pid_t p;
-	int i, status, next, id;
+    pid_t p;
+    int i, status, next, id;
 
     // fprintf(stderr, "SIGCHLD handler: Nproc is %i.\n", nproc);
     // fprintf(stderr, "SIGCHLD called.\n");
@@ -243,37 +243,37 @@ sigchld_handler(int signum)
     }
     // fprintf(stderr, "Round robin scheduler: Waking up %i-th child with processid = %i.\n", current, childid[current]);
     kill(childid[current], SIGCONT);
-	// assert(0 && "Please fill me!");
+    // assert(0 && "Please fill me!");
 }
 
 /* Disable delivery of SIGALRM and SIGCHLD. */
 static void
 signals_disable(void)
 {
-	sigset_t sigset;
+    sigset_t sigset;
 
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGALRM);
-	sigaddset(&sigset, SIGCHLD);
-	if (sigprocmask(SIG_BLOCK, &sigset, NULL) < 0) {
-		perror("signals_disable: sigprocmask");
-		exit(1);
-	}
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGALRM);
+    sigaddset(&sigset, SIGCHLD);
+    if (sigprocmask(SIG_BLOCK, &sigset, NULL) < 0) {
+        perror("signals_disable: sigprocmask");
+        exit(1);
+    }
 }
 
 /* Enable delivery of SIGALRM and SIGCHLD.  */
 static void
 signals_enable(void)
 {
-	sigset_t sigset;
+    sigset_t sigset;
 
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGALRM);
-	sigaddset(&sigset, SIGCHLD);
-	if (sigprocmask(SIG_UNBLOCK, &sigset, NULL) < 0) {
-		perror("signals_enable: sigprocmask");
-		exit(1);
-	}
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGALRM);
+    sigaddset(&sigset, SIGCHLD);
+    if (sigprocmask(SIG_UNBLOCK, &sigset, NULL) < 0) {
+        perror("signals_enable: sigprocmask");
+        exit(1);
+    }
 }
 
 
@@ -284,55 +284,55 @@ signals_enable(void)
 static void
 install_signal_handlers(void)
 {
-	sigset_t sigset;
-	struct sigaction sa;
+    sigset_t sigset;
+    struct sigaction sa;
 
-	sa.sa_handler = sigchld_handler;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGCHLD);
-	sigaddset(&sigset, SIGALRM);
-	sa.sa_mask = sigset;
-	if (sigaction(SIGCHLD, &sa, NULL) < 0) {
-		perror("sigaction: sigchld");
-		exit(1);
-	}
+    sa.sa_handler = sigchld_handler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGCHLD);
+    sigaddset(&sigset, SIGALRM);
+    sa.sa_mask = sigset;
+    if (sigaction(SIGCHLD, &sa, NULL) < 0) {
+        perror("sigaction: sigchld");
+        exit(1);
+    }
 
-	sa.sa_handler = sigalrm_handler;
-	if (sigaction(SIGALRM, &sa, NULL) < 0) {
-		perror("sigaction: sigalrm");
-		exit(1);
-	}
+    sa.sa_handler = sigalrm_handler;
+    if (sigaction(SIGALRM, &sa, NULL) < 0) {
+        perror("sigaction: sigalrm");
+        exit(1);
+    }
 
-	/*
-	 * Ignore SIGPIPE, so that write()s to pipes
-	 * with no reader do not result in us being killed,
-	 * and write() returns EPIPE instead.
-	 */
-	if (signal(SIGPIPE, SIG_IGN) < 0) {
-		perror("signal: sigpipe");
-		exit(1);
-	}
+    /*
+     * Ignore SIGPIPE, so that write()s to pipes
+     * with no reader do not result in us being killed,
+     * and write() returns EPIPE instead.
+     */
+    if (signal(SIGPIPE, SIG_IGN) < 0) {
+        perror("signal: sigpipe");
+        exit(1);
+    }
 }
 
 static void
 do_shell(char *executable, int wfd, int rfd)
 {
-	char arg1[10], arg2[10];
-	char *newargv[] = { executable, NULL, NULL, NULL };
-	char *newenviron[] = { NULL };
+    char arg1[10], arg2[10];
+    char *newargv[] = { executable, NULL, NULL, NULL };
+    char *newenviron[] = { NULL };
 
-	sprintf(arg1, "%05d", wfd);
-	sprintf(arg2, "%05d", rfd);
-	newargv[1] = arg1;
-	newargv[2] = arg2;
+    sprintf(arg1, "%05d", wfd);
+    sprintf(arg2, "%05d", rfd);
+    newargv[1] = arg1;
+    newargv[2] = arg2;
 
-	raise(SIGSTOP);
-	execve(executable, newargv, newenviron);
+    raise(SIGSTOP);
+    execve(executable, newargv, newenviron);
 
-	/* execve() only returns on error */
-	perror("scheduler: child: execve");
-	exit(1);
+    /* execve() only returns on error */
+    perror("scheduler: child: execve");
+    exit(1);
 }
 
 /* Create a new shell task.
@@ -344,32 +344,32 @@ do_shell(char *executable, int wfd, int rfd)
 static pid_t
 sched_create_shell(char *executable, int *request_fd, int *return_fd)
 {
-	pid_t p;
-	int pfds_rq[2], pfds_ret[2];
+    pid_t p;
+    int pfds_rq[2], pfds_ret[2];
 
-	if (pipe(pfds_rq) < 0 || pipe(pfds_ret) < 0) {
-		perror("pipe");
-		exit(1);
-	}
+    if (pipe(pfds_rq) < 0 || pipe(pfds_ret) < 0) {
+        perror("pipe");
+        exit(1);
+    }
 
-	p = fork();
-	if (p < 0) {
-		perror("scheduler: fork");
-		exit(1);
-	}
+    p = fork();
+    if (p < 0) {
+        perror("scheduler: fork");
+        exit(1);
+    }
 
-	if (p == 0) {
-		/* Child */
-		close(pfds_rq[0]);
-		close(pfds_ret[1]);
-		do_shell(executable, pfds_rq[1], pfds_ret[0]);
-		assert(0);
-	}
-	/* Parent */
-	close(pfds_rq[1]);
-	close(pfds_ret[0]);
-	*request_fd = pfds_rq[0];
-	*return_fd = pfds_ret[1];
+    if (p == 0) {
+        /* Child */
+        close(pfds_rq[0]);
+        close(pfds_ret[1]);
+        do_shell(executable, pfds_rq[1], pfds_ret[0]);
+        assert(0);
+    }
+    /* Parent */
+    close(pfds_rq[1]);
+    close(pfds_ret[0]);
+    *request_fd = pfds_rq[0];
+    *return_fd = pfds_ret[1];
 
     return p;
 }
@@ -377,39 +377,39 @@ sched_create_shell(char *executable, int *request_fd, int *return_fd)
 static void
 shell_request_loop(int request_fd, int return_fd)
 {
-	int ret;
-	struct request_struct rq;
+    int ret;
+    struct request_struct rq;
 
-	/*
-	 * Keep receiving requests from the shell.
-	 */
-	for (;;) {
-		if (read(request_fd, &rq, sizeof(rq)) != sizeof(rq)) {
-			perror("scheduler: read from shell");
-			fprintf(stderr, "Scheduler: giving up on shell request processing.\n");
-			break;
-		}
+    /*
+     * Keep receiving requests from the shell.
+     */
+    for (;;) {
+        if (read(request_fd, &rq, sizeof(rq)) != sizeof(rq)) {
+            perror("scheduler: read from shell");
+            fprintf(stderr, "Scheduler: giving up on shell request processing.\n");
+            break;
+        }
 
-		signals_disable();
-		ret = process_request(&rq);
-		signals_enable();
+        signals_disable();
+        ret = process_request(&rq);
+        signals_enable();
 
-		if (write(return_fd, &ret, sizeof(ret)) != sizeof(ret)) {
-			perror("scheduler: write to shell");
-			fprintf(stderr, "Scheduler: giving up on shell request processing.\n");
-			break;
-		}
-	}
+        if (write(return_fd, &ret, sizeof(ret)) != sizeof(ret)) {
+            perror("scheduler: write to shell");
+            fprintf(stderr, "Scheduler: giving up on shell request processing.\n");
+            break;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	int i, child;
+    int i, child;
     char *childargs[] = { NULL }, *environment[] = { NULL };
-	/* Two file descriptors for communication with the shell */
-	static int request_fd, return_fd;
+    /* Two file descriptors for communication with the shell */
+    static int request_fd, return_fd;
 
-	nproc = argc; /* number of proccesses goes here */
+    nproc = argc; /* number of proccesses goes here */
     assert(nproc < PROC_NUM);
     // fprintf(stderr, "nproc is %i.\n", nproc);
     alive = ( int* )malloc(PROC_NUM * sizeof(int));
@@ -420,16 +420,16 @@ int main(int argc, char *argv[])
         procnames[i] = ( char* )malloc(30 * sizeof(char));
     }
 
-	/* Create the shell. */
-	childid[0] = sched_create_shell(SHELL_EXECUTABLE_NAME, &request_fd, &return_fd);
+    /* Create the shell. */
+    childid[0] = sched_create_shell(SHELL_EXECUTABLE_NAME, &request_fd, &return_fd);
     alive[0] = 1;
     procnames[0] = "shell";
     priorities[0] = PRIORITY_LOW;
 
-	/*
-	 * For each of argv[1] to argv[argc - 1],
-	 * create a new child process, add it to the process list.
-	 */
+    /*
+     * For each of argv[1] to argv[argc - 1],
+     * create a new child process, add it to the process list.
+     */
     for (i = 1; i < argc; ++i) {
         child = fork();
         if (child == 0) {
@@ -449,17 +449,17 @@ int main(int argc, char *argv[])
         }
     }
 
-	/* Wait for all children to raise SIGSTOP before exec()ing. */
-	wait_for_ready_children(nproc);
+    /* Wait for all children to raise SIGSTOP before exec()ing. */
+    wait_for_ready_children(nproc);
 
-	if (nproc == 0) {
-		fprintf(stderr, "Scheduler: No tasks. Exiting...\n");
-		exit(1);
-	}
+    if (nproc == 0) {
+        fprintf(stderr, "Scheduler: No tasks. Exiting...\n");
+        exit(1);
+    }
     // fprintf(stderr, "Before installing signal handlers: Nproc is %i.\n", nproc);
 
-	/* Install SIGALRM and SIGCHLD handlers. */
-	install_signal_handlers();
+    /* Install SIGALRM and SIGCHLD handlers. */
+    install_signal_handlers();
 
     alarm(SCHED_TQ_SEC);
 
@@ -468,15 +468,15 @@ int main(int argc, char *argv[])
     // fprintf(stderr, "Before the kill: Nproc is %i.\n", nproc);
     kill(childid[current], SIGCONT);
 
-	shell_request_loop(request_fd, return_fd);
+    shell_request_loop(request_fd, return_fd);
 
-	/* Now that the shell is gone, just loop forever
-	 * until we exit from inside a signal handler.
-	 */
-	while (pause())
-		;
+    /* Now that the shell is gone, just loop forever
+     * until we exit from inside a signal handler.
+     */
+    while (pause())
+        ;
 
-	/* Unreachable */
-	fprintf(stderr, "Internal error: Reached unreachable point\n");
-	return 1;
+    /* Unreachable */
+    fprintf(stderr, "Internal error: Reached unreachable point\n");
+    return 1;
 }
